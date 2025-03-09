73b3dcde5958347e2ccdd7b8565e4d2dcfc118c3
Nima Karimipour <karimipour.nima@gmail.com>
2025-03-09 16:18:23 -0700

    Revert "fix: 1 - METHOD_NO_INIT - public SubscriberMethod( - initializer method does not guarantee @NonNull field methodString (line 28) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 0842d7668c1f3d71bd267601ac4cc522257a3e57.

--- EventBus/src/org/greenrobot/eventbus/SubscriberMethod.java
+++ EventBus/src/org/greenrobot/eventbus/SubscriberMethod.java
@@ -16,8 +16,6 @@
 package org.greenrobot.eventbus;
 
 import java.lang.reflect.Method;
-import java.util.Objects;
-import javax.annotation.Nullable;
 
 /** Used internally by EventBus and generated subscriber indexes. */
 public class SubscriberMethod {
@@ -27,7 +25,7 @@ public class SubscriberMethod {
   final int priority;
   final boolean sticky;
   /** Used for efficient comparison */
-  @Nullable String methodString;
+  String methodString;
 
   public SubscriberMethod(
       Method method, Class<?> eventType, ThreadMode threadMode, int priority, boolean sticky) {
@@ -48,7 +46,7 @@ public class SubscriberMethod {
       otherSubscriberMethod.checkMethodString();
       // Don't use method.equals because of
       // http://code.google.com/p/android/issues/detail?id=7811#c6
-      return Objects.equals(methodString, otherSubscriberMethod.methodString);
+      return methodString.equals(otherSubscriberMethod.methodString);
     } else {
       return false;
     }
