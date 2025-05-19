568c52dca4cb84c9011826540b3c19660555aa39
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:01:22 -0700

    Revert "fix: 8 - DEREFERENCE_NULLABLE - th.printStackTrace(System.out); - dereferenced expression th is @Nullable"
    This reverts commit 0adeb3ecdca6d2d82c6deb5b2518547c6f5ff084.

--- EventBus/src/org/greenrobot/eventbus/Logger.java
+++ EventBus/src/org/greenrobot/eventbus/Logger.java
@@ -54,11 +54,9 @@ public interface Logger {
     }
 
     @Override
-    public void log(Level level, String msg, Throwable th) {
+    public void log(Level level, String msg, @Nullable Throwable th) {
       System.out.println("[" + level + "] " + msg);
-      if (th != null) {
-        th.printStackTrace(System.out);
-      }
+      th.printStackTrace(System.out);
     }
   }
 
