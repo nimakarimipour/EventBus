33a6ba1ba89b07f2573fc703ae1f79404da64169
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 17:01:32 -0700

    Revert "fix: 10 - ASSIGN_FIELD_NULLABLE - clazz = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit e3d85104596ad1cce579d3a73f2d1f2e48aa27bd.

--- EventBus/src/org/greenrobot/eventbus/SubscriberMethodFinder.java
+++ EventBus/src/org/greenrobot/eventbus/SubscriberMethodFinder.java
@@ -243,6 +243,7 @@ class SubscriberMethodFinder {
       subscriberClassByMethodKey.clear();
       methodKeyBuilder.setLength(0);
       subscriberClass = null;
+      clazz = null;
       skipSuperClasses = false;
       subscriberInfo = null;
     }
