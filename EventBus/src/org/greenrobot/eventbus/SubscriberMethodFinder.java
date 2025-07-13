168acff1ec7b17a2c5fde9f489a9043180926826
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:13:37 -0700

    fix: 6 - ASSIGN_FIELD_NULLABLE - clazz = null; - assigning @Nullable expression to @NonNull field

--- EventBus/src/org/greenrobot/eventbus/SubscriberMethodFinder.java
+++ EventBus/src/org/greenrobot/eventbus/SubscriberMethodFinder.java
@@ -288,17 +288,15 @@ class SubscriberMethodFinder {
 
     void moveToSuperclass() {
       if (skipSuperClasses) {
-        clazz = null;
+        clazz = Object.class;
       } else {
         clazz = clazz.getSuperclass();
         String clazzName = clazz.getName();
-        // Skip system classes, this degrades performance.
-        // Also we might avoid some ClassNotFoundException (see FAQ for background).
         if (clazzName.startsWith("java.")
             || clazzName.startsWith("javax.")
             || clazzName.startsWith("android.")
             || clazzName.startsWith("androidx.")) {
-          clazz = null;
+          clazz = Object.class;
         }
       }
     }
